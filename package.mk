efab000a5d604236517b350a96c76529c4b5558a
Christian Döring <chipfunk@gmx.de>
2024-01-01 19:00:18 +0100

    Add pulseaudio-script

--- package.mk
+++ package.mk
@@ -31,4 +31,5 @@ addon() {
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/share/brutefir
   cp -r ${PKG_DIR}/brutefir_defaults ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/share/brutefir
   cp -r ${PKG_DIR}/brutefir_config.example ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/share/brutefir
+  cp -r ${PKG_DIR}/pulseaudio.sh ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/share/brutefir
 }
---
+++ pulseaudio.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# Output to brutefir via FIFO
+pactl load-module module-pipe-sink file=/run/pulse/fifo_output sink_name=brutefir_sink format=s16le rate=44100 channels=2
+pactl set-default-sink brutefir_sink
+
+# Input from brutefir via FIFO
+pactl load-module module-pipe-source file=/run/pulse/fifo_input source_name=brutefir_source format=s16le rate=44100 channels=2
+pactl set-default-source brutefir_source
+
+# Load ALSA card for actual sound-ouptput
+pactl load-module module-alsa-sink device=default
+
+# Route BruteFIR-output to ALSA card
+pactl load-module module-loopback source=brutefir_source sink=alsa_output.default
