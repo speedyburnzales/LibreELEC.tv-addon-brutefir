115c3345f17f7d03296f967e4c6bd8d185e60958
Christian Döring <chipfunk@gmx.de>
2024-01-01 23:02:25 +0100

    Split values on separate lines for pulseaudio

--- pulseaudio.sh
+++ pulseaudio.sh
@@ -1,15 +1,40 @@
 #! /bin/sh
 
 # Output to brutefir via FIFO
-pactl load-module module-pipe-sink file=/run/pulse/fifo_output sink_name=brutefir_sink format=s16le rate=44100 channels=2
-pactl set-default-sink brutefir_sink
+pactl load-module module-pipe-sink \
+    file=/run/pulse/fifo_output \
+    sink_name=brutefir_sink \
+    format=s16le \
+    rate=44100 \
+    channels=2
+
+pactl set-default-sink \
+    brutefir_sink
 
 # Input from brutefir via FIFO
-pactl load-module module-pipe-source file=/run/pulse/fifo_input source_name=brutefir_source format=s16le rate=44100 channels=2
-pactl set-default-source brutefir_source
+pactl load-module module-pipe-source \
+    file=/run/pulse/fifo_input \
+    source_name=brutefir_source \
+    format=s16le \
+    rate=44100 \
+    channels=2
+
+pactl set-default-source \
+    brutefir_source
 
 # Load ALSA card for actual sound-ouptput
-pactl load-module module-alsa-sink device=default
+pactl load-module module-alsa-sink \
+    device=default \
+    format=s16le \
+    rate=44100 \
+    channels=2
 
 # Route BruteFIR-output to ALSA card
-pactl load-module module-loopback source=brutefir_source sink=alsa_output.default
+pactl load-module module-loopback \
+    source=brutefir_source \
+    source_dont_move=true \
+    sink=alsa_output.default \
+    sink_dont_move=true \
+    format=s16le \
+    rate=44100 \
+    channels=2
