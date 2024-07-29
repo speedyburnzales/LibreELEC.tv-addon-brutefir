8450efbd9b5d20d3ad392300af31a7af01ea3cb9
speedyburnzales <38910143+speedyburnzales@users.noreply.github.com>
2024-07-29 18:08:05 +0200

    Update startup.sh
    update mit 2 externen Dacs: USB Audiobox und Impacto

--- startup.sh
+++ startup.sh
@@ -8,7 +8,9 @@ fi
 . /storage/.kodi/addons/brutefir/bin/shutdown.sh
 
 # Provide ALSA output
-pactl load-module module-alsa-sink >> $PULSEAUDIO_MODULES_FILE
+pactl load-module module-alsa-sink device_id=Impacto >> $PULSEAUDIO_MODULES_FILE      # Impacto
+pactl load-module module-alsa-sink device_id=USB >> $PULSEAUDIO_MODULES_FILE      # AudioBox USB
+pactl load-module module-alsa-sink >> $PULSEAUDIO_MODULES_FILE        # Hdmi (default)
 
 # Load null-sink to loopback Kodi-output to BruteFIRs input-stream
 pactl load-module module-null-sink sink_name="BruteFIR" >> $PULSEAUDIO_MODULES_FILE
